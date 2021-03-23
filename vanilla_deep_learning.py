5fb6f4e94a5e02bdc300f4df8d974f54b59a90fa
LevyvoNet <eladlevy2@gmail.com>
2021-03-23 19:44:27 +0200

    kind of done, still to need implement softmax derivative more efficiently (probably more vector operations and less for loops)

---
+++ vanilla_deep_learning.py
@@ -0,0 +1,365 @@
+import typing
+import numpy as np
+import random
+import math
+from keras.datasets import mnist
+from sklearn.model_selection import train_test_split
+
+EPSILON = 1e-7
+
+
+# Forward Propagation ###################################################################################
+
+def initialize_parameters(layer_dims: typing.List):
+    """Initialize the parameters of the neural network.
+
+    Returns:
+        dict. a dictionary with the layers as values.
+        for example:
+        {
+            'W1': [....
+                   ....]
+            'b1': [....]
+        }
+    """
+    params = {}
+
+    for i in range(1, len(layer_dims)):
+        params[f'W{i}'] = np.random.randn(layer_dims[i], layer_dims[i - 1]) * np.sqrt(1. / layer_dims[i - 1])
+        params[f'b{i}'] = np.zeros(([layer_dims[i], 1])) * np.sqrt(1. / layer_dims[i - 1])
+
+    return params
+
+
+def linear_forward(A_prev: np.array, W: np.array, b: np.array):
+    """Process the previous layer with the wights of the next one, only the linear part.
+
+    The result of this computation will go into the activation of the current layer.
+    Save the received parameters for the chain rule in backward propgation later
+    """
+    Z = np.matmul(W, A_prev) + b
+    linear_cache = {'A_prev': A_prev, 'W': W, 'b': b}
+
+    return Z, linear_cache
+
+
+def softmax(Z: np.array):
+    return np.exp(Z) / np.sum(np.exp(Z), axis=0), Z
+
+
+def relu(Z: np.array):
+    return np.maximum(Z, 0), Z
+
+
+ACTIVATION_STR_TO_FUNC = {
+    'relu': relu,
+    'softmax': softmax
+
+}
+
+
+def linear_activation_forward(A_prev, W, b, activation_func_str):
+    activation_func = ACTIVATION_STR_TO_FUNC[activation_func_str]
+    Z, cache = linear_forward(A_prev, W, b)
+    A, _ = activation_func(Z)
+
+    cache.update({'Z': Z})
+
+    return A, cache
+
+
+def apply_batchnorm(A):
+    """Apply batch normalization on the output of a layer (which is the input of the next one)"""
+    A_mean = np.sum(A) / A.shape[0]
+    A_var = np.sum((A - A_mean) ** 2) / A.shape[0]
+    A_bn = (A - A_mean) / np.sqrt((A_var + 1e-7))
+
+    return A_bn
+
+
+def L_model_forward(X, parameters, use_batchnorm):
+    if len(parameters) % 2 != 0:
+        raise ValueError(f"Parameters length should be even because there is W and b for every layer, "
+                         "got {len(parameters)} instead}")
+
+    caches = []
+    layers_count = int(len(parameters) / 2)
+
+    # Perform the forward propagation for all of the middle-hidden layers
+    A = X
+    for layer_idx in range(1, layers_count):
+        W = parameters[f'W{layer_idx}']
+        b = parameters[f'b{layer_idx}']
+        A, cache = linear_activation_forward(A, W, b, 'relu')
+
+        if use_batchnorm:
+            A = apply_batchnorm(A)
+
+        caches.append(cache)
+
+    # Perform the output layer - softmax
+    W = parameters[f'W{layers_count}']
+    b = parameters[f'b{layers_count}']
+    AL, cache = linear_activation_forward(A, W, b, 'softmax')
+    caches.append(cache)
+
+    return AL, caches
+
+
+def compute_cost(AL, Y):
+    """Compute cross-entropy cost
+
+    Args:
+        AL(n_classes,n_examples)
+        Y(n_classes, n_examples)
+    """
+    m = Y.shape[1]
+
+    # Select the likelihoods predicted for the true labels for each sample
+    log_likelihood = np.multiply(np.log(AL), Y)
+
+    return (-1. / m) * np.sum(log_likelihood)
+
+
+# Backward Propagation ##################################################################################
+
+def linear_backward(dZ, cache):
+    A_prev = cache['A_prev']
+    W = cache['W']
+
+    m = dZ.shape[1]
+    dA_prev = np.matmul(W.T, dZ)
+    dW = (1. / m) * np.matmul(dZ, A_prev.T)
+    db = (1. / m) * np.sum(dZ, axis=1, keepdims=True)
+
+    return {
+        'dA_prev': dA_prev,
+        'dW': dW,
+        'db': db,
+    }
+
+
+def softmax_derivative(cache):
+    """Calculate the derivative of Softmax
+
+    Args:
+        cache (dict). The cache contains W, b, A_prev, Z of the requested layer.
+
+    Returns:
+        dg. The derivative of the Softmax (dg(Z))
+    """
+
+    jacobian = np.zeros(A.shape)
+
+    for col in range(A.shape[1]):
+        for row in range(A.shape[0]):
+            # For each
+            dg[row][col] = A[row][col] * (1 - A[row][col])
+
+    dg_same = np.multiply(A, 1 - A)
+    dg_different = np.multiply()
+
+    # TODO: add dg for the non-same-index part (-y_i*y_j)
+
+    return dg
+
+
+def relu_backward(dA, cache):
+    """dA is basically W*dZ"""
+    Z = cache['Z']
+
+    dg = Z[:]
+    dg[Z <= 0] = 0
+    dg[Z > 0] = 1
+
+    dZ = np.multiply(dA, dg)
+    return dZ
+
+
+def softmax_backward(dA, cache):
+    Z = cache['Z']
+    # TODO: maybe save A in cache instead of re-computing?
+    A = softmax(Z)[0]
+
+    # Calculate the partial derivatives of each of the softmax output with respect
+    # to each of the z_i variables
+    m = A.shape[1]
+
+    # for each instance, compute its jacobian and then its dZ
+    dZ = np.zeros((A.shape[0], m))
+    for m_i in range(m):
+
+        # Compute the jacobian of the instance softmax
+        jacobian = np.zeros((A.shape[0], A.shape[0]))
+        a = A[:, m_i]
+        for i in range(len(jacobian)):
+            for j in range(len(jacobian)):
+                if i == j:
+                    jacobian[i][j] = a[i] * (1 - a[i])
+                else:
+                    jacobian[i][j] = -a[i] * a[j]
+
+        # Compute dZ of the instance
+        for j in range(len(jacobian)):
+            dZ[j][m_i] = sum([dA[k, m_i] * jacobian[k, j]
+                              for k in range(len(jacobian))])
+
+    return dZ
+
+
+ACTIVATION_BACKWARD_STR_TO_FUNC = {
+    'relu': relu_backward,
+    'softmax': softmax_backward
+}
+
+
+def linear_activation_backward(dA, cache, activation):
+    dZ = ACTIVATION_BACKWARD_STR_TO_FUNC[activation](dA, cache)
+
+    return linear_backward(dZ, cache)
+
+
+def L_model_backward(AL, Y, caches):
+    """Compute the gradients for each layer"""
+    layer_count = len(caches)
+    grads = {}
+
+    # Calculate the derivatives of the loss function in terms of AL - the output of the final layer
+    dAL = -np.divide(Y, AL)
+
+    # Perform the back propagation for the final, softmax layer
+    grads["dA{layer_count}"] = dAL
+    curr_grads = linear_activation_backward(dAL, caches[-1], 'softmax')
+    grads[f'dW{layer_count}'] = curr_grads['dW']
+    grads[f'db{layer_count}'] = curr_grads['db']
+
+    # Perform the backpropagation for all of the middle, RELU layers
+    for i in range(layer_count - 1, 0, -1):
+        grads["dA{i}"] = curr_grads['dA_prev']
+        curr_grads = linear_activation_backward(grads["dA{i}"], caches[i - 1], 'relu')
+        grads[f'dW{i}'] = curr_grads['dW']
+        grads[f'db{i}'] = curr_grads['db']
+
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate):
+    if len(parameters) % 2 != 0:
+        raise ValueError(f'parameters length should be even, got {len(parameters)} instead')
+
+    n_layers = int(len(parameters) / 2)
+    for i in range(1, n_layers + 1):
+        parameters[f'W{i}'] -= learning_rate * grads[f'dW{i}']
+        parameters[f'b{i}'] -= learning_rate * grads[f'db{i}']
+
+    # NOTE: this is actually redundant since the given parameters is an object which we are changing anyway
+    return parameters
+
+
+def generate_batches(X, Y, batch_size):
+    """Generate random batches of the given size from the given instances.
+
+    Args:
+        X(n_features, n_samples): The input of the neural network
+        batch_size (int): the desired batch size
+
+    Return:
+        generator. The batches in an iterable manner (one can execute a for loop on it).
+        each element will contain batch from X and its matching elements from Y
+    """
+    m = X.shape[1]
+    indices = list(range(m))
+    random.shuffle(indices)
+    n_batches = int(math.ceil(m / batch_size))
+    for i in range(n_batches):
+        X_batch = X[:, indices[i * batch_size:(i + 1) * batch_size]]
+        Y_batch = Y[:, indices[i * batch_size:(i + 1) * batch_size]]
+
+        yield X_batch, Y_batch
+
+
+def train_model(params, X, Y, learning_rate, num_iterations, batch_size):
+    """Train the given parameters more times (assuming RELU for all middle layers and softmax for the last one)"""
+    for epoch_idx in range(num_iterations):
+        epoch_total_cost = 0
+        for batch_idx, (X_batch, Y_batch) in enumerate(generate_batches(X, Y, batch_size)):
+            AL, caches = L_model_forward(X_batch, params, use_batchnorm=False)
+
+            # Calculate batch cost
+            cost = compute_cost(AL, Y_batch)
+            epoch_total_cost += cost
+
+            grads = L_model_backward(AL, Y_batch, caches)
+            params = update_parameters(params, grads, learning_rate)
+
+        print(f'epoch total cost:{epoch_total_cost}')
+        if (epoch_idx + 1) % 100 == 0:
+            print(f'Training cost is {epoch_total_cost}')
+            costs.append(epoch_total_cost)
+
+    return params, costs
+
+
+def L_layer_model(X, Y, layers_dims, learning_rate, num_iterations, batch_size):
+    params = initialize_parameters(layers_dims)
+
+    return train_model(params, X, Y, learning_rate, num_iterations, batch_size)
+
+
+def predict(X, Y, parameters):
+    AL, _ = L_model_forward(X, parameters, False)
+    outputs = np.argmax(AL, axis=0)
+    y = np.argmax(Y, axis=0)
+
+    all = len(outputs)
+    correct = len(outputs[outputs == y])
+
+    return correct / all
+
+
+if __name__ == '__main__':
+    (train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+    # float 0-1
+    train_X = train_X.reshape(train_X.shape[0], 784).astype('float32') / 255.
+    test_X = test_X.reshape(test_X.shape[0], 784).astype('float32') / 255.
+
+    # 1hot encoding
+    y_enc = np.zeros((len(train_y), 10), np.float32)
+    y_enc[range(y_enc.shape[0]), train_y] = 1
+
+    # train_test_split
+    X_tr, X_val, y_tr, y_val = train_test_split(train_X, y_enc, test_size=0.2, random_state=42)
+
+    # transpose
+    X_tr = X_tr.T
+    X_te = X_val.T
+    y_tr = y_tr.T
+    y_te = y_val.T
+
+    # Train until there is no significant improvement on the validation set ##############################
+
+    # Train for first 100 iterations
+    model_params, costs = L_layer_model(X_tr, y_tr, [784, 20, 7, 5, 10], 0.009, 100, 512)
+    acc = predict(X_val, y_val, model_params)
+    delta = 100
+
+    # Keep training the model until the improvement is less than 1 percent
+    while delta > 1:
+        model_params, new_costs = train_model(model_params, X_tr, y_tr, 0.009, 100, 64)
+        costs += new_costs
+
+        new_acc = predict(X_te, y_te, model_params)
+        delta = ((new_acc - acc) / acc) * 100
+        print(f'new accuracy: {new_acc}, previous accuracy: {acc}')
+        acc = new_acc
+
+    # Test the model on the test set
+    test_acc = predict(X_te, y_te, model_params)
+
+    # Print stats
+    for i, cost in enumerate(costs):
+        print(f'cost of {(i + 1) * 100} step is {cost}')
+
+    print(f'final accuracy on validation set is {acc}')
+    print(f'accuracy on test set is {test_acc}')
